4e713794aad20d6854cec75ea841c81243133d7d
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2025-07-20 18:51:25 +0530

    margin modified for body

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-      <footer className="bg-[#252752] text-white py-6 text-center text-sm font-medium">
+      <footer className="text-black py-6 text-center text-sm font-medium">
         &copy; {new Date().getFullYear()} AJ Studio. All rights reserved.
       </footer>
     );
